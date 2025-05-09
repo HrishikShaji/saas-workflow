daba50256ba4b8d400bca12699b26d1779376ec1
Hrishik Shaji <hrishikminishaji@gmail.com>
2025-05-09 11:11:09 +0530

    minor ui chnages

--- components/workflow/ExecutionViewer.tsx
+++ components/workflow/ExecutionViewer.tsx
@@ -64,60 +64,62 @@ export default function ExecutionViewer({ initialData }: Props) {
 
 	const creditsConsumed = getPhasesTotalCost(data?.phases || [])
 
-	return <div className="flex w-full h-full">
-		<aside className="w-[300px] min-w-[300px] max-w-[300px] border-r-2 border-seperate flex flex-grow flex-col overflow-hidden">
-			<div className="py-4 px-2">
-				<ExecutionLabel
-					icon={CircleDashedIcon}
-					label="Status"
-					value={data?.status}
-				/>
-				<ExecutionLabel
-					icon={CalendarIcon}
-					label="Started at"
-					value={data?.startedAt ? formatDistanceToNow(new Date(data.startedAt), { addSuffix: true }) : "-"}
-				/>
-				<ExecutionLabel
-					icon={ClockIcon}
-					label="Duration"
-					value={duration ? duration : <Loader className="animate-spin " size={20} />}
-				/>
-				{/*
+	return <div className="w-full h-[calc(100vh_-_110px)]">
+		<div className="w-full h-[50px]  flex gap-5 border-b-[1px] border-gray-300">
+
+			<ExecutionLabel
+				icon={CircleDashedIcon}
+				label="Status"
+				value={data?.status}
+			/>
+			<ExecutionLabel
+				icon={CalendarIcon}
+				label="Started at"
+				value={data?.startedAt ? formatDistanceToNow(new Date(data.startedAt), { addSuffix: true }) : "-"}
+			/>
+			<ExecutionLabel
+				icon={ClockIcon}
+				label="Duration"
+				value={duration ? duration : <Loader className="animate-spin " size={20} />}
+			/>
+			{/*
 				<ExecutionLabel
 					icon={CoinsIcon}
 					label="Credits consumed"
 					value={creditsConsumed}
 				/>
 				*/}
-			</div>
-			<Separator />
-			<div className="flex justify-center items-center py-2 px-4">
-				<div className="text-muted-foreground flex items-center gap-2">
-					<WorkflowIcon size={20} className="stroke-muted-foreground" />
-					<span className="font-semibold">Phases</span>
+		</div>
+		<div className="w-full h-full flex">
+
+			<aside className="w-[300px] min-w-[300px] max-w-[300px] border-r-2 border-seperate flex flex-grow flex-col overflow-hidden">
+				<div className="flex justify-center items-center py-2 px-4">
+					<div className="text-muted-foreground flex items-center gap-2">
+						<WorkflowIcon size={20} className="stroke-muted-foreground" />
+						<span className="font-semibold">Phases</span>
+					</div>
 				</div>
-			</div>
-			<Separator />
-			<div className="overflow-auto h-full px-2 py-4 flex flex-col gap-1">
-				{data?.phases.map((phase, i) => (
-					<Button key={phase.id} className="w-full justify-between"
-						onClick={() => {
-							//if (isRunning) return;
-							setSelectedPhase(phase.id)
-						}}
-						style={{ background: selectedPhase === phase.id ? "blue" : "" }}
-					>
-						<div className="flex items-center gap-2">
-							<Badge variant="outline" className="text-white">{i + 1}</Badge>
-							<p className="font-semibold text-white">{phase.name}</p>
-						</div>
-						<PhaseStatusBadge status={phase.status as ExecutionPhaseStatus} />
-					</Button>
-				))}
-			</div>
-		</aside>
-		<div className="flex w-full h-full p-5">
-			{/*
+				<Separator />
+				<div className="overflow-auto h-full px-2 py-4 flex flex-col gap-1">
+					{data?.phases.map((phase, i) => (
+						<Button key={phase.id} className="w-full justify-between"
+							onClick={() => {
+								//if (isRunning) return;
+								setSelectedPhase(phase.id)
+							}}
+							style={{ background: selectedPhase === phase.id ? "blue" : "" }}
+						>
+							<div className="flex items-center gap-2">
+								<Badge variant="outline" className="text-white">{i + 1}</Badge>
+								<p className="font-semibold text-white">{phase.name}</p>
+							</div>
+							<PhaseStatusBadge status={phase.status as ExecutionPhaseStatus} />
+						</Button>
+					))}
+				</div>
+			</aside>
+			<div className="flex w-full h-full p-5">
+				{/*
 					{isRunning && (
 				<div className="flex items-center flex-col gap-2 justify-center h-full w-full">
 					<p className="font-bold">Run is in Progress</p>
@@ -135,10 +137,10 @@ export default function ExecutionViewer({ initialData }: Props) {
 			)}
 
 		*/}
-			{selectedPhase && phaseDetails.data && (
-				<div className="flex flex-col py-4 container gap-4 overflow-auto">
-					<div className="flex gap-2 items-center">
-						{/*
+				{selectedPhase && phaseDetails.data && (
+					<div className="flex flex-col py-4 container gap-4 overflow-auto">
+						<div className="flex gap-2 items-center">
+							{/*
 						<Badge variant="outline" className="space-x-4">
 							<div className="flex gap-1 items-center">
 								<CoinsIcon size={18} className="stroke-muted-foreground" />
@@ -148,33 +150,34 @@ export default function ExecutionViewer({ initialData }: Props) {
 							</div>
 						</Badge>
 						*/}
-						<Badge variant="outline" className="space-x-4">
-							<div className="flex gap-1 items-center">
-								<ClockIcon size={18} className="stroke-muted-foreground" />
-								<span>Duration</span>
-								<span>{datesToDurationString(phaseDetails.data.completedAt, phaseDetails.data.startedAt) || "-"}</span>
+							<Badge variant="outline" className="space-x-4">
+								<div className="flex gap-1 items-center">
+									<ClockIcon size={18} className="stroke-muted-foreground" />
+									<span>Duration</span>
+									<span>{datesToDurationString(phaseDetails.data.completedAt, phaseDetails.data.startedAt) || "-"}</span>
 
-							</div>
-						</Badge>
+								</div>
+							</Badge>
+						</div>
+						<ParameterViewer
+							type="input"
+							title="Inputs"
+							subTitle="Inputs used for this phase"
+							paramJSON={phaseDetails.data.inputs}
+						/>
+						<ParameterViewer
+							type="output"
+							title="Outputs"
+							subTitle="Outputs used for this phase"
+							paramJSON={phaseDetails.data.outputs}
+						/>
+						<LogViewer
+							logs={phaseDetails.data.logs}
+						/>
 					</div>
-					<ParameterViewer
-						type="input"
-						title="Inputs"
-						subTitle="Inputs used for this phase"
-						paramJSON={phaseDetails.data.inputs}
-					/>
-					<ParameterViewer
-						type="output"
-						title="Outputs"
-						subTitle="Outputs used for this phase"
-						paramJSON={phaseDetails.data.outputs}
-					/>
-					<LogViewer
-						logs={phaseDetails.data.logs}
-					/>
-				</div>
-			)}
+				)}
 
+			</div>
 		</div>
 	</div>
 }
@@ -183,7 +186,7 @@ export default function ExecutionViewer({ initialData }: Props) {
 function ExecutionLabel({ icon, label, value }: { icon: LucideIcon; label: ReactNode; value: ReactNode }) {
 	const Icon = icon
 	return (
-		<div className="text-sm flex justify-between items-center py-2 px-4">
+		<div className="text-sm flex justify-between gap-3 items-center py-2 px-4">
 			<div className="text-muted-foreground flex items-center gap-2">
 				<Icon size={20} className="stroke-muted-foreground/80" />
 				<span>{label}</span>
